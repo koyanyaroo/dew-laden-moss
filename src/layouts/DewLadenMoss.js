626339df646a161203d16dcb04fd5374d40be8ee
notaspecial <notaspecial@gmail.com>
2022-06-16 20:42:00 +0700

    update font

--- src/layouts/DewLadenMoss.js
+++ src/layouts/DewLadenMoss.js
@@ -132,9 +132,9 @@ export default function DewLadenMoss({children}) {
                 {/* Static sidebar for desktop */}
                 <div className="hidden md:flex md:w-60 md:flex-col md:fixed md:inset-y-0 ">
                     {/* Sidebar component, swap this element with another sidebar if you like */}
-                    <div className="flex flex-col flex-grow border-r border-brand/10 pt-5 bg-white overflow-y-auto">
+                    <div className="flex flex-col flex-grow border-r border-brand/10 pt-4 bg-white overflow-y-auto">
                         <div className="flex items-center flex-shrink-0 px-6 space-x-2">
-                            <svg className="w-6 h-6" style={{"shapeRendering":"geometricPrecision","textRendering":"geometricPrecision","imageRendering":"optimizeQuality","fillRule":"evenodd","clipRule":"evenodd"}} viewBox="0 0 1270 1270" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
+                            <svg className="w-8 h-8" style={{"shapeRendering":"geometricPrecision","textRendering":"geometricPrecision","imageRendering":"optimizeQuality","fillRule":"evenodd","clipRule":"evenodd"}} viewBox="0 0 1270 1270" xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve">
                                 <defs/>
                                 <g id="Layer_x0020_1">
                                     <g id="_2763128350256">
@@ -149,16 +149,16 @@ export default function DewLadenMoss({children}) {
                                     </g>
                                 </g>
                             </svg>
-                            <div className="font-medium text-lg">Dew Laden Moss</div>
+                            <div className="font-semibold text-sm">Dew Laden Moss</div>
                         </div>
-                        <div className="mt-8 flex-grow flex flex-col">
+                        <div className="mt-10 flex-grow flex flex-col">
                             <nav className="flex-1 px-4 pb-4 space-y-1">
                                 {navigation.map((item) => (
                                     <a
                                         key={item.name}
                                         href={item.href}
                                         className={classNames(
-                                            item.current ? 'bg-brand/10 text-gray-800' : 'text-gray-600 hover:bg-brand/10 hover:text-gray-800',
+                                            item.current ? 'bg-brand/10 text-brand' : 'text-gray-600 hover:bg-brand/10',
                                             'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                                         )}
                                     >
